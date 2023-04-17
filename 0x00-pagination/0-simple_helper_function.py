6e73bebbca5095296a5dffbe4b984a52b1a40030
safarilewis <lleewwii1@gmail.com>
2023-04-17 20:22:06 +0300

    fixing checker fail

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''Function that takes two args and returns pagination params'''
     first = (page - 1) * page_size
-    last = start + page_size
+    last = first + page_size
     return (first, last)
