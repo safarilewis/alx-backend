def73e268f69b96f2fba21deeb5de1b0507eb01f
safarilewis <lleewwii1@gmail.com>
2023-04-17 20:42:23 +0300

    fixed typo

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -33,7 +33,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''Returns data after pagination'''
         assert type(page) == int and type(page_size) == int
-        assert (page_size > 0 and page_size > 0)
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
         if start > len(data):
