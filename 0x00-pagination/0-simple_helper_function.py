c16db5df3c0ca5be54a4865620cb8b5f166aa653
safarilewis <lleewwii1@gmail.com>
2023-04-17 20:15:51 +0300

    task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''A simple helper function'''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''Function that takes two args and returns pagination params'''
+    first = (page - 1) * page_size
+    last = start + page_size
+    return (first, last)
