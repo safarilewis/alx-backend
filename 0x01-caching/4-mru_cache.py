898ea785d0b7f29fb15ef39ff36c35860ea98572
safarilewis <lleewwii1@gmail.com>
2023-04-19 15:07:46 +0300

    mru first try

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''MRU caching system'''
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    '''Implements a MRU caching system'''
+
+    def __init__(self):
+        '''Initializes the cache'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''Puts an item and removes the last item if cache is full'''
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                poppedKey, itemPopped = self.cache_data.popitem(True)
+                print('DISCARD: {}'.format(poppedKey))
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, True)
+
+    def get(self, key):
+        '''Gets the item tied to key in cache and moves it to last'''
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key, True)
+            return self.cache_data.get(key)
