2803a44dd4c1fda403fed91dbda1619f3a22d41f
safarilewis <lleewwii1@gmail.com>
2023-05-23 13:49:10 +0300

    formatting and docstring

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,15 @@
-from flask import Flask
-from flask import render_template
+#!/usr/bin/env python3
+'''Flask app basic'''
+from flask import Flask, render_template
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/')
-def welcome():
+
+@app.route('/',)
+def welcome() -> str:
+    '''renders a template on the route'''
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
