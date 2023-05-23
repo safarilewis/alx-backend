9bba181e0c2f6630111670ce84fc3d24b5dcdd6f
safarilewis <lleewwii1@gmail.com>
2023-05-24 01:29:33 +0300

    task upload

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''flask app'''
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """flask-babel config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """The index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
